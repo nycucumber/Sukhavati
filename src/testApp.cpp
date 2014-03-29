7d2f559c67f359bab021f92076c8058175f8ec30
Yang Wang <yang.wang.dec@gmail.com>
2014-03-29 02:35:38 -0400

    oculusrift camera not moving at all…

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -4,7 +4,7 @@
 void testApp::setup(){
     
     //general
-    ofBackground(0, 0, 0);
+    //ofBackground(0, 0, 0);
     ofSetVerticalSync(true);
     ofToggleFullscreen();
     ofSetLogLevel(OF_LOG_VERBOSE);
@@ -16,11 +16,14 @@ void testApp::setup(){
     ambientLight.setPosition(lightPos);
     
     //camera
-    camPos.set(ofVec3f(1152,1572,0));
-    cam.lookAt(ofVec3f(0,400,200));
+    
+    cam.setPosition(ofVec3f(0,-1000,0));
+    //cam.lookAt(ofVec3f(0,1,0), ofVec3f(0,1,0));
+    
     cam.begin();
     cam.end();//Don't we need to draw stuff while camera is actived?
-   
+    
+    
     //3d model
     roomModel.loadModel("room.3ds");
     roomModel.setPosition(0, 0, 0);
@@ -38,18 +41,18 @@ void testApp::setup(){
     
     //osculus rift
     oculusRift.baseCamera = &cam;
+    
     oculusRift.setup();
     
     //text
-    zero.loadFont("Helvetica.dfont", 20);
+    zero.loadFont("Helvetica.dfont", 5);
     
-    }
+}
 
 //--------------------------------------------------------------
 void testApp::update(){
     
     ofBackground(0);
-    oculusRift.baseCamera = &cam;
     if(oculusRift.isSetup()){
         ofRectangle viewport  = oculusRift.getOculusViewport();
     }
@@ -63,59 +66,49 @@ void testApp::update(){
 
 //--------------------------------------------------------------
 void testApp::draw(){
+    ambientLight.enable();
     
-    ofSetColor(255);
-    zero.drawString("0,0", 0, 0);
+    ofSetColor(255, 255, 255);
+    glEnable(GL_DEPTH_TEST);
+    oculusRift.beginLeftEye();
+    drawScene();
+    oculusRift.endLeftEye();
+    oculusRift.beginRightEye();
+    drawScene();
+    oculusRift.endRightEye();
     
-    if(oculusRift.isSetup()){
-        ofSetColor(255, 255, 255);
-        glEnable(GL_DEPTH_TEST);
-        oculusRift.beginLeftEye();
-		drawScene();
-		oculusRift.endLeftEye();
-		oculusRift.beginRightEye();
-		drawScene();
-		oculusRift.endRightEye();
-		oculusRift.draw();
-		glDisable(GL_DEPTH_TEST);//ANYBODY TELL ME WHAT DOES THIS DO?
-        
-        //////////PUSH MATRIX//////////////
-        ofPushMatrix();
-        ofScale(10, 10);
-        roomModel.draw();
-        ofPopMatrix();
-        //////////POP MATRIX//////////////
-
-        
-    }
-	else{
-		cam.begin();
-		drawScene();
-		cam.end();
-
-    }
+    oculusRift.draw();
+    glDisable(GL_DEPTH_TEST);//ANYBODY TELL ME WHAT DOES THIS DO?
+    
+    //////////PUSH MATRIX//////////////
+    ofPushMatrix();
+    ofScale(10, 10);
+    roomModel.draw();
+    ofPopMatrix();
+    //////////POP MATRIX//////////////
+    ambientLight.disable();
+    
+    
 }
 //--------------------------------------------------------------
 
 void testApp::drawScene()
 {
 	
-//	ofPushMatrix();
-//	ofRotate(90, 0, 0, -1);
-//	ofDrawGridPlane(500.0f, 10.0f, false );
-//	ofPopMatrix();
-//	
-	ofPushStyle();
-    
-    if(oculusRift.isSetup()){
-        
-    //////////PUSH MATRIX//////////////
+    //	ofPushMatrix();
+    //	ofRotate(90, 0, 0, -1);
+    //	ofDrawGridPlane(500.0f, 10.0f, false );
+    //	ofPopMatrix();
+    //
+	    if(oculusRift.isSetup()){
+       
+        //////////PUSH MATRIX//////////////
 		ofPushMatrix();
         //Don't know what is this...
 		oculusRift.multBillboardMatrix();
         //Draw Room
         ofNoFill();
-        ofDrawBox(0, ofGetHeight()/2, -800, 2400,2400,4000 );
+        ofDrawBox(0, 0, -800, 2400,3000,4000 );
         //coordinate
         ofSetColor(255, 0, 0);
         ofLine(0, 0, 0, 800, 0,0);
@@ -124,6 +117,9 @@ void testApp::drawScene()
         ofSetColor(0, 0, 255);
         ofLine(0, 0, 0, 0, 0, 800);
         ofPopStyle();
+        ofSetColor(255);
+        zero.drawString("0,0,0", 0, 0);
+        ofDrawGrid();
         //DRAW THE 1ST KINECT IMAGE
         ofSetColor(255,255,255);
         drawPointCloud();
@@ -133,11 +129,9 @@ void testApp::drawScene()
         drawAnotherPointCloud();
         ofPopStyle();
 		ofPopMatrix();
+        //////////POP MATRIX//////////////
         
-    //////////POP MATRIX//////////////
-
-        
-	}
+        }
 	
 	
     
@@ -172,7 +166,7 @@ void testApp::drawPointCloud(){
 	ofDisableDepthTest();
 	ofPopMatrix();
     //////////PUSH MATRIX//////////////
-
+    
     
     
     
@@ -188,7 +182,7 @@ void testApp::drawAnotherPointCloud() {
 	for(int y = 0; y < h; y += step) {
 		for(int x = 0; x < w; x += step) {
 			if(kinect2.getDistanceAt(x, y) > 0 && kinect2.getDistanceAt(x,y) < 1200) {
-				mesh2.addColor(kinect2.getColorAt(x,y));
+				//mesh2.addColor(kinect2.getColorAt(x,y));
 				mesh2.addVertex(kinect2.getWorldCoordinateAt(x, y));
 			}
 		}
@@ -203,7 +197,7 @@ void testApp::drawAnotherPointCloud() {
 	ofDisableDepthTest();
 	ofPopMatrix();
     //////////PUSH MATRIX//////////////
-
+    
 }
 #endif
 
@@ -230,18 +224,18 @@ void testApp::keyPressed(int key){
             
         case 'n':
             angle--;
-           if(angle<-30)angle=-30;
+            if(angle<-30)angle=-30;
             kinect.setCameraTiltAngle(angle);
-           break;
+            break;
         case 'm':
             angle++;
             if(angle>30)angle=30;
             kinect.setCameraTiltAngle(angle);
             break;
             
-        //modify camera position by x,y,z key
+            //modify camera position by x,y,z key
         case 'x':
-           camPos += ofVec3f(step,0,0);
+            camPos += ofVec3f(step,0,0);
         case 'X':
             camPos += ofVec3f(-step,0,0);
         case 'y':
@@ -249,54 +243,54 @@ void testApp::keyPressed(int key){
         case 'Y':
             camPos += ofVec3f(0,-step,0);
         case 'z':
-           camPos += ofVec3f(0,0,step);
+            camPos += ofVec3f(0,0,step);
         case 'Z':
-           camPos += ofVec3f(0,0,-step);
+            camPos += ofVec3f(0,0,-step);
     }
     
-  
     
     
-     ofLog()<<"cam pos: "<<cam.getPosition();
-
+    
+    ofLog()<<"cam pos: "<<cam.getPosition();
+    
 }
 
 //--------------------------------------------------------------
 void testApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::dragEvent(ofDragInfo dragInfo){ 
-
+    
 }
